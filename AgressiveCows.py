b9cefc3407086c2e2105b91a29262b33f00cd29f
udaykumar48402 <udaykumarchavvakula@gmail.com>
2025-02-17 18:11:34 +0530

    Create AgressiveCows.py

---
+++ AgressiveCows.py
@@ -0,0 +1,28 @@
+class Solution:
+    def aggressiveCows(self, stalls, k):
+        stalls.sort()
+        low, high = 1, stalls[-1] - stalls[0]
+        while low <= high:
+            mid = (low + high) // 2
+            count, last_placed = 1, stalls[0]
+            for i in range(1, len(stalls)):
+                if stalls[i] - last_placed >= mid:
+                    count += 1
+                    last_placed = stalls[i]
+            if count >= k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
+import bisect
+if __name__ == '__main__':
+    t = int(input())
+    while t:
+        t -= 1
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ans = ob.aggressiveCows(A, D)
+        print(ans)
+        print("~")
